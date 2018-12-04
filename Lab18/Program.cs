129b1f8f2894293851a72188c77e4d501a1e9597
Kristy Currier <kristy.currier@gmail.com>
2018-12-04 15:50:48 -0500

    InsertAt method added

--- Lab18/Program.cs
+++ Lab18/Program.cs
@@ -31,20 +31,46 @@ namespace Lab18
                 }
                 boolValue = true;
             }
-
             return boolValue;
         }
 
         public static void PrintReverse<t>(this LinkedList<t> list)
         {
-            for (int i = list.Count-1; i >=0; i--)
+            for (int i = list.Count - 1; i >= 0; i--)
             {
                 Console.WriteLine(list.ElementAt(i));
             }
         }
+
+        public static bool InsertAt<t>(this LinkedList<t> list, int index, t o)
+        {
+            var length = list.Count;
+            var boolValue = false;
+            if (index >= length)
+            {
+                boolValue = false;
+            }
+            else
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    if (i == index)
+                    {
+                        list.AddLast(o);
+                    }
+                    list.AddLast(list.ElementAt(i));
+                }
+                for (int i = 0; i < length; i++)
+                {
+                    list.RemoveFirst();
+                }
+                boolValue = true;
+            }
+            return boolValue;
+
+        }
+
     }
-
-
     class Program
     {
         static void Main(string[] args)
@@ -65,7 +91,8 @@ namespace Lab18
                 Console.WriteLine(list.ElementAt(i));
             }
 
-            bool validIndex = list.RemoveAt(7);
+            //bool validIndex = list.RemoveAt(7);
+            bool validIndex = list.InsertAt(2, 20);
             Console.WriteLine("");
 
             for (int i = 0; i < list.Count; i++)
@@ -74,7 +101,7 @@ namespace Lab18
             }
 
             Console.WriteLine(validIndex);
-            list.PrintReverse();
+            //list.PrintReverse();
 
             Console.ReadKey();
         }
